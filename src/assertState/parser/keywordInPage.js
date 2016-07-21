d8b19227adbad5a1a2a1b5359bbd164e56ed3a9a
loveKino <842914439@qq.com>
2016-07-21 23:53:28 +0800

    text module

--- src/assertState/parser/keywordInPage.js
+++ src/assertState/parser/keywordInPage.js
@@ -1,5 +1,9 @@
 'use strict';
 
+let {
+    getDisplayText
+} = require('page-text');
+
 /**
  * keyword that page contains
  */
@@ -11,45 +15,5 @@ module.exports = (keywords) => {
         if (text.indexOf(keyword) === -1) {
             throw new Error(`missing keyword: ${keyword} in page`);
         }
-    }
-};
-
-let getDisplayText = (node) => {
-    if (node.nodeType === 3) {
-        return node.textContent || '';
-    }
-
-    if (node.nodeType !== 1) {
-        return '';
-    }
-
-    if(node.tagName === 'INPUT') {
-        return node.value || '';
-    }
-
-    if (node.tagName === 'HEAD' ||
-        node.tagName === 'LINK' ||
-        node.tagName === 'SCRIPT' ||
-        node.tagName === 'NOSCRIPT' ||
-        node.tagName === 'CANVAS' ||
-        node.tagName === 'STYLE' ||
-        node.tagName === 'META') {
-        return '';
-    }
-
-    let style = window.getComputedStyle(node);
-    let display = style.display;
-
-    if (display === 'none') {
-        return '';
-    } else {
-        let text = '';
-        let children = node.childNodes;
-        for (let i = 0; i < children.length; i++) {
-            let child = children[i];
-            text += getDisplayText(child);
-        }
-
-        return text;
     }
 };
